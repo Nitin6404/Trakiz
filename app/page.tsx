3c7b377d3ca6d87c4174ab1758329c52f909bedc
nitinnennn <nc1575612@gmail.com>
2024-07-01 20:45:33 +0530

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import { Main } from "@/components/main";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Trakiz',
+  description: 'Trakiz is a simple, fast, and secure way to track your habits and goals.',
+  icons: "/logooftrakiz.png",
+}
 
 export default function Home() {
   return (
